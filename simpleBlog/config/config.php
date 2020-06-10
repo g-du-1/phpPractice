d1adc3e818d7d914186718be459080cd20114828
Gergo Dudaszeg <dudaszeg1@gmail.com>
2020-06-10 18:49:19 +0100

    Remove files

--- simpleBlog/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('ROOT_URL', 'http://localhost/phpPractice/simpleBlog/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'debian-sys-maint');
-    define('DB_PASS', 'lQD9Ch8k5mdXV7EZ');
-    define('DB_NAME', 'phpblog');
-?>
